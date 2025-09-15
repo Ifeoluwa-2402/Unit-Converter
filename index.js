7569ff0fea7c0cc6e1d4bb22a88c1d74b450965d
Ifeoluwa <ifeoluwaolugbode2402@gmail.com>
2025-09-15 10:15:59 +0100

    Updated my JS file
    Updated the code so when the input is empty it will give an alert

--- index.js
+++ index.js
@@ -6,32 +6,34 @@ const mass = document.getElementById("mass")
 function convertLength(value) {
     const meter  = value * 0.3048
     const feet =  value * 3.28084
-    const result = `${value} meters = ${feet.toFixed(3)} feet | ${value} feet = ${meter.toFixed(3)} meters`
+    const result = `${value} meters = ${feet.toFixed(3)} feet |
+                    ${value} feet = ${meter.toFixed(3)} meters`
     length.textContent = result
 }
 function convertVolume(value) {
     const gallon  = value * 0.264172
     const liter =  value * 3.78541
-    const result = `${value} liters = ${gallon.toFixed(3)} gallons | ${value} gallon = ${liter.toFixed(3)} liters`
+    const result = `${value} liters = ${gallon.toFixed(3)} gallons | 
+                    ${value} gallon = ${liter.toFixed(3)} liters`
     volume.textContent = result
 }
 function convertMass(value) {
     const pound  = value * 2.20462
     const kilo =  value* 0.453592
-    const result = `${value} kilos = ${pound.toFixed(3)} pounds | ${value} pounds = ${kilo.toFixed(3)} kilos`
+    const result = `${value} kilos = ${pound.toFixed(3)} pounds | 
+                    ${value} pounds = ${kilo.toFixed(3)} kilos`
     mass.textContent = result
 }
 convert.addEventListener('click', function() {
-    // if(input.value === "") {
-    //     alert("Please enter a number")
-    // } else if (isNaN(input.value)) {
-    //     alert("Invalid input. Please enter a valid number.")
-    //     input.value = ""
-    //     return
-    // }
+    if(input.value === "") {
+        alert("Please enter a number")
+    } else if (isNaN(input.value)) {
+        alert("Invalid input. Please enter a valid number.")
+        input.value = ""
+        return
+    }
     convertLength(input.value)
     convertVolume(input.value)
     convertMass(input.value)
     input.value = ""
 })
-
